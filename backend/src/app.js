72d68a7fc89e1b7f62246dd478e5420a92e16449
Ana Melem <anamelem4@gmail.com>
2023-07-23 02:34:54 -0300

    Backend conection created

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const router = require('./router');
 
 const app = express();
+
+app.use(router);
 
 app.get('/', (request, response) =>response.status(200).send('Olá, Mundo!') )
 
