531919f25c4fef9ef608f64be68b47e49c6dee30
Ana Melem <anamelem4@gmail.com>
2023-07-25 04:56:47 -0300

    API with cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 app.get('/', (request, response) =>response.status(200).send('Olá, Mundo!') )
 
